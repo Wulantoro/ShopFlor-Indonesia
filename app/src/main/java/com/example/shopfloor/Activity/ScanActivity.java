43afc96346a7ec5f83b476b44175e72135b75aca
wulantoro <nugrahawulantoro@gmail.com>
2019-08-19 16:39:46 +0700

    edit scan barcode 19-08-19

--- app/src/main/java/com/example/shopfloor/Activity/ScanActivity.java
+++ app/src/main/java/com/example/shopfloor/Activity/ScanActivity.java
@@ -1,49 +1,105 @@
 package com.example.shopfloor.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
 
+import com.example.shopfloor.Models.Productorder;
 import com.example.shopfloor.R;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.zxing.Result;
 
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
-    private ZXingScannerView mScannerView;
+public class ScanActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
 
-    @Override
+    BarcodeReader barcodeReader;
+    private Productorder productorder;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
-        mScannerView = new ZXingScannerView(this);
-        setContentView(mScannerView);
-    }
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
 
-    public void onResume() {
-        super.onResume();
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera();
-    }
-
-    public void onPause() {
-        super.onPause();
-        mScannerView.stopCamera();
     }
 
     @Override
-    public void handleResult(Result rawResult) {
-        Log.v("TAG", rawResult.getText()); // Prints scan results
-        Log.v("TAG", rawResult.getBarcodeFormat().toString());
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scan Result");
-        builder.setMessage(rawResult.getText());
-        AlertDialog alert1 = builder.create();
-        alert1.show();
+    public void onScanned(Barcode barcode) {
 
-        mScannerView.resumeCameraPreview(this);
+        Intent intent = new Intent(ScanActivity.this, Add_DocActivity.class);
+        intent.putExtra("wccode", barcode.displayValue);
+        startActivity(intent);
+
+    }
+
+
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String s) {
+        Toast.makeText(getApplicationContext(), "Error occurred while scanning " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        finish();
     }
 }
+
+
+
+//public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+//
+//    private ZXingScannerView mScannerView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_scan);
+//
+//        mScannerView = new ZXingScannerView(this);
+//        setContentView(mScannerView);
+//    }
+//
+//    public void onResume() {
+//        super.onResume();
+//        mScannerView.setResultHandler(this);
+//        mScannerView.startCamera();
+//    }
+//
+//    public void onPause() {
+//        super.onPause();
+//        mScannerView.stopCamera();
+//    }
+//
+//    @Override
+//    public void handleResult(Result rawResult) {
+//        Log.v("TAG", rawResult.getText()); // Prints scan results
+//        Log.v("TAG", rawResult.getBarcodeFormat().toString());
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Scan Result");
+//        builder.setMessage(rawResult.getText());
+//        AlertDialog alert1 = builder.create();
+//        alert1.show();
+//
+//        mScannerView.resumeCameraPreview(this);
+//    }
+//}
---
+++ app/src/main/java/com/example/shopfloor/Models/Poscan.java
@@ -0,0 +1,183 @@
+package com.example.shopfloor.Models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+
+public class Poscan implements Parcelable
+{
+
+    private int uSequence;
+    private String uQuantity;
+    private String itemCode;
+    private int DocNum;
+    private String status;
+    private String itemName;
+    private String uDesc;
+    private String uIsActive;
+    private String code;
+    private String name;
+    private String wccode;
+    private String plannedQty;
+    private String uWCCode;
+    public final static Parcelable.Creator<Poscan> CREATOR = new Creator<Poscan>() {
+
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public Poscan createFromParcel(Parcel in) {
+            return new Poscan(in);
+        }
+
+        public Poscan[] newArray(int size) {
+            return (new Poscan[size]);
+        }
+
+    }
+            ;
+
+    protected Poscan(Parcel in) {
+        this.uSequence = ((int) in.readValue((int.class.getClassLoader())));
+        this.uQuantity = ((String) in.readValue((String.class.getClassLoader())));
+        this.itemCode = ((String) in.readValue((String.class.getClassLoader())));
+        this.DocNum = ((int) in.readValue((int.class.getClassLoader())));
+        this.status = ((String) in.readValue((String.class.getClassLoader())));
+        this.itemName = ((String) in.readValue((String.class.getClassLoader())));
+        this.uDesc = ((String) in.readValue((String.class.getClassLoader())));
+        this.uIsActive = ((String) in.readValue((String.class.getClassLoader())));
+        this.code = ((String) in.readValue((String.class.getClassLoader())));
+        this.name = ((String) in.readValue((String.class.getClassLoader())));
+        this.wccode = ((String) in.readValue((String.class.getClassLoader())));
+        this.plannedQty = ((String) in.readValue((String.class.getClassLoader())));
+        this.uWCCode = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    public Poscan() {
+    }
+
+    public int getUSequence() {
+        return uSequence;
+    }
+
+    public void setUSequence(int uSequence) {
+        this.uSequence = uSequence;
+    }
+
+    public String getUQuantity() {
+        return uQuantity;
+    }
+
+    public void setUQuantity(String uQuantity) {
+        this.uQuantity = uQuantity;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public int getDocNum() {
+        return DocNum;
+    }
+
+    public void setDocNum(int docNum) {
+        this.DocNum = docNum;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getUDesc() {
+        return uDesc;
+    }
+
+    public void setUDesc(String uDesc) {
+        this.uDesc = uDesc;
+    }
+
+    public String getUIsActive() {
+        return uIsActive;
+    }
+
+    public void setUIsActive(String uIsActive) {
+        this.uIsActive = uIsActive;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWccode() {
+        return wccode;
+    }
+
+    public void setWccode(String wccode) {
+        this.wccode = wccode;
+    }
+
+    public String getPlannedQty() {
+        return plannedQty;
+    }
+
+    public void setPlannedQty(String plannedQty) {
+        this.plannedQty = plannedQty;
+    }
+
+    public String getUWCCode() {
+        return uWCCode;
+    }
+
+    public void setUWCCode(String uWCCode) {
+        this.uWCCode = uWCCode;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(uSequence);
+        dest.writeValue(uQuantity);
+        dest.writeValue(itemCode);
+        dest.writeValue(DocNum);
+        dest.writeValue(status);
+        dest.writeValue(itemName);
+        dest.writeValue(uDesc);
+        dest.writeValue(uIsActive);
+        dest.writeValue(code);
+        dest.writeValue(name);
+        dest.writeValue(wccode);
+        dest.writeValue(plannedQty);
+        dest.writeValue(uWCCode);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+}
